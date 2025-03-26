5eaa385e2acda1ee269c2d81b2fd16ace218bd29
Easer <>
2025-03-26 20:05:39 +0700

    添加swagger注释

--- modules/cache_module/routes.py
+++ modules/cache_module/routes.py
@@ -4,4 +4,17 @@ cache_bp = Blueprint('cache', __name__, url_prefix='/cache')
 
 @cache_bp.route('/status')
 def cache_status():
+    """获取缓存模块状态
+    ---
+    tags:
+      - Cache
+    responses:
+      200:
+        description: 缓存模块状态信息
+        schema:
+          properties:
+            status:
+              type: string
+              description: 模块运行状态
+    """
     return {'status': 'Cache module is running'}
